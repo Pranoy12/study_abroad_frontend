2d1bdfd9719a858c3f07ff79d935f59c2a2e6394
Pranoy12 <pranoyderil23@gmail.com>
2025-06-27 23:24:40 +0530

    Login page enhanced

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -32,7 +32,15 @@ const Home = () => {
   return (
     <>
       <div>
-        <button onClick={() => logout()}>Logout</button>
+        <button
+          className="text-black bg-white-700 hover:bg-white-800 focus:ring-4 
+              focus:outline-none focus:ring-white-300 font-medium rounded-lg 
+              text-sm px-5 py-2.5 text-center dark:bg-white-600 
+              dark:hover:bg-white-700 dark:focus:ring-whitex-800"
+          onClick={() => logout()}
+        >
+          Logout
+        </button>
       </div>
       <form onSubmit={handleSubmit}>
         <div className="grid gap-6 mb-6 md:grid-cols-2">
@@ -137,15 +145,17 @@ const Home = () => {
             .
           </label>
         </div>
-        <button
-          type="submit"
-          className="ml-110 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
-            focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
-            text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 
-            dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+              focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+              text-sm px-5 py-2.5 text-center dark:bg-blue-600 
+              dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </>
   );
